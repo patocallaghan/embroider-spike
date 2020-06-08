57542db055a37c8c25ae05b236480f4c60348938
Gavin Joyce <gavinjoyce@gmail.com>
2020-06-08 12:45:45 +0100

    @embroider/router

--- app/router.js
+++ app/router.js
@@ -1,13 +1,13 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
+import EmberRouter from "@embroider/router";
+import config from "./config/environment";
 
 export default class Router extends EmberRouter {
   location = config.locationType;
   rootURL = config.rootURL;
 }
 
-Router.map(function() {
-  this.route('players');
-  this.route('teams');
-  this.route('table');
+Router.map(function () {
+  this.route("players");
+  this.route("teams");
+  this.route("table");
 });
