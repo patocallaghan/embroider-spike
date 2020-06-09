96da68eefa44e834623751be4a88c9fe4a840144
Gavin Joyce <gavinjoyce@gmail.com>
2020-06-09 10:23:04 +0100

    add ember-concurrency

---
+++ app/components/addons/ember-concurrency.js
@@ -0,0 +1,13 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+import { task } from "ember-concurrency-decorators";
+import { timeout } from "ember-concurrency";
+
+export default class AddonsEmberConcurrencyComponent extends Component {
+  @tracked count = 0;
+
+  @task *doSomething() {
+    yield timeout(1000);
+    this.count++;
+  }
+}
