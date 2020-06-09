ddeeef53afe2328f63652f4ba11e212a364f9d36
Gavin Joyce <gavinjoyce@gmail.com>
2020-06-09 10:55:25 +0100

    simple-set-helper

---
+++ app/components/addons/ember-simple-set-helper.js
@@ -0,0 +1,6 @@
+import Component from "@glimmer/component";
+import { tracked } from "@glimmer/tracking";
+
+export default class AddonsEmberSimpleSetHelperComponent extends Component {
+  @tracked isExpanded = false;
+}
